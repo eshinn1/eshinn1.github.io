6fb67676b20ded2a23442579946127427e534d9a
eshinn1 <60208402+eshinn1@users.noreply.github.com>
2020-04-15 13:52:56 -0400

    jquery

--- hw/jquery/main.js
+++ hw/jquery/main.js
@@ -1,2 +1,29 @@
 $(document).ready(function() {
   console.log("we loaded");
+
+$( "h1" ).css("color", "purple");
+
+$( "#button1")
+.click( function() {
+  alert("www.google.com");
+
+  $( "h1" )
+  .addclass("big-text");
+
+
+} );
+
+$( "p" )
+.mouseover( function() {
+$("p")
+.css("font-size", "21px");
+
+} );
+
+$( "p" )
+.mouseleave( function() {
+  $( "p" )
+  .css("background-color", "white" );
+  $( "p" )
+  .css( "font-size", "16px" );
+}
