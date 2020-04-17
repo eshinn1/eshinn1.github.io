2e90bbef96da02a551f199f768ce926556d36d8a
eshinn1 <60208402+eshinn1@users.noreply.github.com>
2020-04-16 22:01:57 -0400

    push

--- hw/jquery/main.js
+++ hw/jquery/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   console.log("we loaded");
 
 $("h1").click("click", function() {
-  alert("Hey!");
+  alert("Scroll down for the recipe! Enjoy!");
   $(this).css("background-color", "red");
 });
 
@@ -13,6 +13,10 @@ $( "h1" ).addClass("big-text");
 
 $("#ingredients").click("click", function(){
   $(this).css("background-color", "white");
+});
+
+$("h1").click(function() {
+  $("h1").html("<em>The Best Cinnamon Rolls</em>");
 });
 
 $( "p" )
